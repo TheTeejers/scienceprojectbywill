7150140436f10ed1e7ba74374b58c6f23eead5ac
TJ Loughry <tjloughry@gmail.com>
2018-01-06 12:16:23 -0600

    finished with survey TODO is add results page

---
+++ js/getResults.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_FssRS8PtRnUZds2eyV3BD4LToT26fo4",
+  authDomain: "petcostproject.firebaseapp.com",
+  databaseURL: "https://petcostproject.firebaseio.com",
+  projectId: "petcostproject",
+  storageBucket: "petcostproject.appspot.com",
+  messagingSenderId: "805579558495"
+};
+  firebase.initializeApp(config);
+  database = firebase.database();
